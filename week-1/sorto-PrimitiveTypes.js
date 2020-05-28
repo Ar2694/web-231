e288941d1ad53bafab6c3940c208b264e36210ff
Arlix Sorto <arlix2694@gmail.com>
2020-05-27 20:00:13 -0500

    Assigment 1.4 - Primitive Types

---
+++ week-1/sorto-PrimitiveTypes.js
@@ -0,0 +1,34 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Professor Krasso
+; Date: 27 May 2020
+; Modified By: Arlix Sorto
+; Description: This program demostrate the variable declarations,
+:              Variable assignments, and outputs.
+;===========================================
+*/ 
+
+/* Variable declarations */
+var firstName;
+var lastName;
+var age;
+var street;
+var city;
+var zipCode;
+
+/* Variable assignments */
+firstName = "Arlix";
+lastName = "Sorto";
+age = "25";
+street = "123 sesame street";
+city = "Omaha";
+zipCode = "68803";
+
+/* Output */
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
