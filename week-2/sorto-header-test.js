d1a6fd757e02d203f0af8df7ae95819d42f0104a
Arlix Sorto <arlix2694@gmail.com>
2020-06-03 21:21:22 -0500

    Week 2 folder and files

---
+++ week-2/sorto-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  sorto-header-test.js
+; Author: Arlix Sorto
+; Date:   6/3/2020
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../sorto-header.js');
+console.log(header.display("Arlix", "Sorto", "Exercise 2.2 - Header"));
+
