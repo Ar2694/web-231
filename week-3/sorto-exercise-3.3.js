6c8b0885dc905628f0c5a264f4150d7906e2b4f6
Arlix Sorto <arlix2694@gmail.com>
2020-06-09 23:38:34 -0500

    Changes

--- week-3/sorto-exercise-3.3.js
+++ week-3/sorto-exercise-3.3.js
@@ -9,21 +9,6 @@
   The enter key was pressed
 */
 
-let eventKeyCode = 13
-
-if (eventKeyCode === 13) {
-  console.log('The enter key was pressed.')
-} else if (eventKeyCode === 16) {
-  console.log('The shift key was pressed.')
-} else if (eventKeyCode === 32) {
-  console.log('The spacebar key was pressed.')
-} else if (eventKeyCode === 8) {
-  console.log('The backspace / delete key was pressed.')
-} else {
-  console.log('Unrecognized key.')
-}
-
-
 // Switch statement
 switch(eventKeyCode) {
 
