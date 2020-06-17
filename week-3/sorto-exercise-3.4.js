d71ceff6b6f667dce965814cd05baeb5274562a1
Arlix Sorto <arlix2694@gmail.com>
2020-06-17 00:03:26 -0500

    Changes

--- week-3/sorto-exercise-3.4.js
+++ week-3/sorto-exercise-3.4.js
@@ -1,8 +1,10 @@
 /*
+  // Below is what I am expecting to see when I run your program - remove this before submitting your work
+  // 50% of you will not read this and lose 25% of the awardable points.
 
-  FirstName LastName
+  Arlix Sorto
   Assignment 3.4
-  Today's Date
+  6/12/2020
 
   -- DO THE NUMBERS MATCH GAME --
   6 does not match 4!
@@ -22,15 +24,7 @@
  * Disclaimer: Do not remove this function as this is what you will be using to generate a random number
  */
 
-/* Import sorto-header.js from the root directory*/
-const header = require('../sorto-header.js');
-
-/** Calls the display function and output the header*/
-console.log(header.display("Arlix", "Sorto", "Assignment 3.4"));
-console.log("");
-
-
-var myNumber = 6;
+var myNumber = 5;
 
 console.log(" -- DO THE NUMBERS MATCH GAME --");
 
