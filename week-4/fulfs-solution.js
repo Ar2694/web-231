dfa3fe6d328e6e8ee9252b926290ce12ba567455
Arlix Sorto <arlix2694@gmail.com>
2020-06-20 10:06:41 -0500

    fulfs solution

---
+++ week-4/fulfs-solution.js
@@ -0,0 +1,23 @@
+/*
+ 	============================================
+ 	; Title: Discussion 4.1
+ 	; Author: Perry Fulfs
+ 	; Date: 16 June 2020
+ 	; Modified By: Perry Fulfs
+ 	; Description: create an array
+ 	; that outputs random motorcycle brand name
+ 	;===========================================
+ 	*/
+
+ 	// output: random motorcyle brand
+ 	// FIXME:
+
+ 	function random_item(items)
+ 	{
+
+ 	return items[Math.floor(Math.random()*items.length)];
+
+ 	}
+
+ 	var items = ["YAMAHA", "HONDA", "TRIUMPH", "HARLEY DAVIDSON", "DUCATI"];
+ 	console.log(random_item(items));
